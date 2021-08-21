e437f919e65ee77b5ab8597b3be6abc46e1726e7
frvalenzuela <frvalenzuela@uc.cl>
2021-08-21 18:27:54 -0400

    filter-with-selector

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import { display_page, remove_pages, add_button } from "./renders.js";
-import { getAllPokemonData,  filter_by_type, filter_by_gen, addListBtnMore, addListByType, addListByGen } from "./services.js";
+import { getAllPokemonData,  filter_by_type, filter_by_gen, addListBtnMore, addListByType, addListByGen, addListFilt } from "./services.js";
 
 
 const page = [0];
@@ -17,12 +17,12 @@ display_page(data, html_element);
 // data = await filter_by_type('fire');
 // display_page(data, html_element);
 
-add_button('.filters', 'filter-by-type');
-addListByType(data, html_element, page, url, '.filter-by-type');
+//add_button('.filters', 'filter-by-type');
+//addListByType(data, html_element, page, url, '.filter-by-type');
 
 
-add_button('.filters', 'filter-by-gen');
-addListByGen(data, html_element, page, url, '.filter-by-gen');
+//add_button('.filters', 'filter-by-gen');
+//addListByGen(data, html_element, page, url, '.filter-by-gen');
 
 // Click fuera del modal, cerrarlo
 window.onclick = function(event) {
@@ -32,3 +32,23 @@ window.onclick = function(event) {
 }
 
 addListBtnMore(data, html_element, page, quantity, url);
+
+let selector_type = document.getElementById("type");
+let selector_gen = document.getElementById("gen");
+
+selector_type.addEventListener("change", function() {
+    selector_gen.selectedIndex = 0;
+    console.log(selector_type.value);
+});
+
+addListByType(data, html_element, page, url, 'type');
+
+selector_gen.addEventListener("change", function() {
+    selector_type.selectedIndex = 0;
+    console.log(selector_gen.value);
+});
+
+addListByGen(data, html_element, page, url, 'gen');
+
+
+// addListFilt(data, html_element, page, url, selector_gen, "generation");
