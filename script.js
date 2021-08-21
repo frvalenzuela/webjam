db93796b0e0d2844c6e7b6b1e64187e67779db33
frvalenzuela <frvalenzuela@uc.cl>
2021-08-21 16:33:10 -0400

    finish-filter

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import { display_page, remove_pages, add_button } from "./renders.js";
-import { getAllPokemonData,  filter_by_type, filter_by_gen, addListBtnMore } from "./services.js";
+import { getAllPokemonData,  filter_by_type, filter_by_gen, addListBtnMore, addListByType, addListByGen } from "./services.js";
 
 
 const page = [0];
@@ -9,24 +9,26 @@ let url_start = 'https://pokeapi.co/api/v2/'
 let query = 'pokemon';
 let range_url = `?offset=${start}&limit=${quantity}`;
 const url = [url_start, query, range_url];
-let data = await getAllPokemonData(url[0] + url[1] + url[2]);
+const data = await getAllPokemonData(url[0] + url[1] + url[2]);
 const html_element = [];
 
 display_page(data, html_element);
-remove_pages(html_element, page);
+// remove_pages(html_element, page);
 // data = await filter_by_type('fire');
 // display_page(data, html_element);
 
 add_button('.filters', 'filter-by-type');
+addListByType(data, html_element, page, url, '.filter-by-type');
 
+
+add_button('.filters', 'filter-by-gen');
+addListByGen(data, html_element, page, url, '.filter-by-gen');
 
 // Click fuera del modal, cerrarlo
 window.onclick = function(event) {
     if (event.target === document.getElementById("modal")) {
-      document.getElementById("modal").style.display = "none";
+        document.getElementById("modal").style.display = "none";
     }
 }
 
 addListBtnMore(data, html_element, page, quantity, url);
-
-const array_h1 = [];
